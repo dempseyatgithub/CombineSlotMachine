3bb533c107738558ad91f8f0d7d449016fe29209
James Dempsey <dempsey@tapas-software.net>
2019-06-22 06:08:17 -0700

    Initial Commit

---
+++ CombineSlotMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CombineSlotMachine
+//
+//  Created by James Dempsey on 6/22/19.
+//  Copyright © 2019 James Dempsey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
